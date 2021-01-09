19a9b47964fc26b09bb84bd99d65dd2eaff46a73
Lauren Anders <laurenanders02@gmail.com>
2021-01-08 21:32:57 -0500

    init commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.Locale.LanguageRange;
+
+public class Main {
+    public static void main(String[] args) {
+        //not this line, type below
+        System.out.println("Hello World!");
+        //not this line, type above
+        int age = 18;
+        String name = "Lauren";
+        String lastName = "Anders";
+        int pets = 3;
+        String favoriteBook = "Name of the Wind";
+        int plants = 6;
+        System.out.println(pets + plants);
+        System.out.println(name + " " + lastName);
+        System.out.println(name + " " + lastName + " has " + pets + " pets.");
+    }
+}
